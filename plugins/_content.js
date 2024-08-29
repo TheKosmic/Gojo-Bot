c548845b4ff517107b21c0c56d03fe3fe67a36a7
TheKosmic <158140790+TheKosmic@users.noreply.github.com>
2024-08-28 22:37:22 -0400

    Update _content.js

--- plugins/_content.js
+++ plugins/_content.js
@@ -13,8 +13,8 @@ global.welgata = [tk, ig, yt2, yt2, ig, md, ig, yt, paypal, yt2, yt2, ig, fb, tk
 global.redesMenu = [canal1, canal2, canal3, canal4, soporteGB, grupo1, grupo2, grupo3, grupo4, grupo5, grupo6, md, ig, paypal, yt, asistencia, fb, tk].getRandom()
 global.accountsgb = [canal1, canal2, canal3, canal4, tk, ig, yt, paypal, fb, ths, md, asistencia].getRandom()
 
-global.canalIdGB = ["120363160031023229@newsletter", "120363169294281316@newsletter", "120363203805910750@newsletter", "120363302472386010@newsletter"]
-global.canalNombreGB = ["INFINITY-WA 💫", "GB - UPDATE 🐈", "Tips sobre GataBot 🤩", "NEW PROJECT: YartexBot-MD ✨"]
+global.canalIdGB = ["120363236880225319@newsletter", "120363236880225319@newsletter", "120363236880225319@newsletter", "120363236880225319@newsletter"]
+global.canalNombreGB = ["THE KOSMIC", "THE KOSMIC", "THE KOSMIC", "THE KOSMIC"]
 global.channelRD = await getRandomChannel()
 
 // Imágenes 
